a4f43d0830813972c57407db21de8baaaeda7554
juanodsk <juanph.dv@gmail.com>
2024-10-18 10:30:34 -0500

    Implementacion de header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Header from '../src/assets/Components/Header.jsx'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>,
 )
