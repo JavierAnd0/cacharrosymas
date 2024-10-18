c28812638110e5cbcdcad6189ae4812b203876ad
ConnorA0 <logijavier@gmail.com>
2024-10-18 13:10:40 -0500

    comit cambio de main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from '../src/assets/Components/Header.jsx'
+import Header from '../src/assets/Components/Header.jsx';
+import Nav from '../src/assets/Components/nav.jsx';
+
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Nav />
   </StrictMode>,
 )
