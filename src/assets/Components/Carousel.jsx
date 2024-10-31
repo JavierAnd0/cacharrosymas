a7e451a040dc7eaac34d4260fd9f6747e5e58c93
ConnorA0 <144062573+JavierAnd0@users.noreply.github.com>
2024-10-31 12:02:50 -0500

    producto

--- src/assets/Components/Carousel.jsx
+++ src/assets/Components/Carousel.jsx
@@ -6,33 +6,39 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 
 const ImageCarousel = () => (
   <CarouselProvider
-    naturalSlideWidth={450}  // Ancho natural del slide (ajusta según tu diseño)
-    naturalSlideHeight={300} // Alto natural del slide (ajusta según tu diseño)
+    naturalSlideWidth={1600}  // Ancho natural del slide para un monitor grande
+    naturalSlideHeight={900}  // Alto natural del slide para pantalla completa
     totalSlides={3}
     isPlaying={true}
     interval={2000}
     infinite={true}
     touchEnabled={true}
     dragEnabled={true}
-    style={{ width: "100%", maxWidth: "900px", margin: "0 auto" }} // Tamaño del contenedor
+    style={{
+      width: "100vw",      // Ancho completo de la ventana
+      height: "60vh",     // Alto completo de la ventana
+      maxWidth: "100%",    
+      margin: "0 auto",    
+      overflow: "hidden",  
+    }}
   >
     <Slider>
       <Slide tag="a" index={0}>
         <Image 
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fiainsuranceconsultants.com%2Fwp-content%2Fuploads%2F2015%2F05%2F800x800.png&f=1&nofb=1&ipt=0148426d4f0b999f1fa3ae57f93b2f0be244f04600c02765726f35185f59d05d&ipo=images" 
-          style={{ width: "100%", height: "auto", objectFit: "cover" }} // Ajuste de la imagen
+          style={{ width: "100%", height: "100%", objectFit: "cover" }} // Ajuste de la imagen
         />
       </Slide>
       <Slide tag="a" index={1}>
         <Image 
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fiainsuranceconsultants.com%2Fwp-content%2Fuploads%2F2015%2F05%2F800x800.png&f=1&nofb=1&ipt=0148426d4f0b999f1fa3ae57f93b2f0be244f04600c02765726f35185f59d05d&ipo=images" 
-          style={{ width: "100%", height: "auto", objectFit: "cover" }} // Ajuste de la imagen
+          style={{ width: "100%", height: "100%", objectFit: "cover" }} // Ajuste de la imagen
         />
       </Slide>
       <Slide tag="a" index={2}>
         <Image 
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fiainsuranceconsultants.com%2Fwp-content%2Fuploads%2F2015%2F05%2F800x800.png&f=1&nofb=1&ipt=0148426d4f0b999f1fa3ae57f93b2f0be244f04600c02765726f35185f59d05d&ipo=images" 
-          style={{ width: "100%", height: "auto", objectFit: "cover" }} // Ajuste de la imagen
+          style={{ width: "100%", height: "100%", objectFit: "cover" }} // Ajuste de la imagen
         />
       </Slide>
     </Slider>
